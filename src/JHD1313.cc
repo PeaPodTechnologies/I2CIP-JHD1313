61dd317697845c34f82e93fbdb2b63262d5dfc56
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2024-12-31 20:00:55 -0500

    Refactor JHD1313 Src

--- src/JHD1313.cc
+++ src/JHD1313.cc
@@ -1,70 +1,74 @@
-#include <PCA9632.h>
+#include "JHD1313.h"
 
-#ifndef PCA9632_ENABLE_CURSOR
-#define PCA9632_ENABLE_CURSOR false
+#include <debug.h>
+
+#ifndef JHD1313_ENABLE_CURSOR
+#define JHD1313_ENABLE_CURSOR false
 #endif
 
-#ifndef PCA9632_ENABLE_BLINK
-#define PCA9632_ENABLE_BLINK false
+#ifndef JHD1313_ENABLE_BLINK
+#define JHD1313_ENABLE_BLINK false
 #endif
 
-#define PCA9632_FAILSAFE_ARGS { .r = 0x3F, .g = 0xFF, .b = 0x3F } // White
+#define JHD1313_FAILSAFE_ARGS { .r = 0x3F, .g = 0xFF, .b = 0x3F } // White
 
-I2CIP_DEVICE_INIT_STATIC_ID(PCA9632);
-I2CIP_OUTPUT_INIT_FAILSAFE(PCA9632, String, String("[I2CIP] HELLO!"), i2cip_pca9632_args_t, PCA9632_FAILSAFE_ARGS);
+I2CIP_DEVICE_INIT_STATIC_ID(JHD1313);
+I2CIP_OUTPUT_INIT_FAILSAFE(JHD1313, String, String("[I2CIP] HELLO!"), i2cip_jhd1313_args_t, JHD1313_FAILSAFE_ARGS);
 
-// bool PCA9632::_failsafe_set = false;
-// char PCA9632::_failsafe[PCA9632_TXMAX] = {"I2CIP\nBLANK"};
-// uint16_t PCA9632::_failsafe_b = PCA9632_FAILSAFE_ARGS;
-
-PCA9632::PCA9632(i2cip_fqa_t fqa, const i2cip_id_t& id) : I2CIP::Device(fqa, id), I2CIP::OutputInterface<String, i2cip_pca9632_args_t>((I2CIP::Device*)this), Print(), fqa_rgb(I2CIP::createFQA(I2CIP_FQA_SEG_I2CBUS(fqa), I2CIP_FQA_SEG_MODULE(fqa), I2CIP_FQA_SEG_MUXBUS(fqa), PCA9632_ADDRESS_RGBLED)) { }
-// PCA9632::PCA9632(i2cip_fqa_t fqa, const i2cip_id_t& id) : I2CIP::Device(fqa, id), I2CIP::OutputInterface<const char*, i2cip_pca9632_args_t>((I2CIP::Device*)this), fqa_rgb(I2CIP::createFQA(I2CIP_FQA_SEG_I2CBUS(fqa), I2CIP_FQA_SEG_MODULE(fqa), I2CIP_FQA_SEG_MUXBUS(fqa), PCA9632_ADDRESS_RGBLED)) { }
+// bool JHD1313::_failsafe_set = false;
+// char JHD1313::_failsafe[JHD1313_TXMAX] = {"I2CIP\nBLANK"};
+// uint16_t JHD1313::_failsafe_b = JHD1313_FAILSAFE_ARGS;
 
 using namespace I2CIP;
 
-i2cip_errorlevel_t PCA9632::begin(uint8_t cols, uint8_t rows, bool setbus) {
-  if(cols > PCA9632_COLS || rows > PCA9632_ROWS) {
+JHD1313::JHD1313(i2cip_fqa_t fqa, const i2cip_id_t& id) : Device(fqa, id), OutputInterface<String, i2cip_jhd1313_args_t>((Device*)this), Print(), fqa_rgb(createFQA(I2CIP_FQA_SEG_I2CBUS(fqa), I2CIP_FQA_SEG_MODULE(fqa), I2CIP_FQA_SEG_MUXBUS(fqa), JHD1313_ADDRESS_RGBLED)) { }
+// JHD1313::JHD1313(i2cip_fqa_t fqa, const i2cip_id_t& id) : Device(fqa, id), OutputInterface<const char*, i2cip_jhd1313_args_t>((Device*)this), fqa_rgb(createFQA(I2CIP_FQA_SEG_I2CBUS(fqa), I2CIP_FQA_SEG_MODULE(fqa), I2CIP_FQA_SEG_MUXBUS(fqa), JHD1313_ADDRESS_RGBLED)) { }
+
+using namespace I2CIP;
+
+i2cip_errorlevel_t JHD1313::begin(uint8_t cols, uint8_t rows, bool setbus) {
+  if(cols > JHD1313_COLS || rows > JHD1313_ROWS) {
     return I2CIP_ERR_SOFT;
   }
 
   i2cip_errorlevel_t errlev; // uint8_t c = 0;
   // do{
   //   c++;
-    errlev = writeRegister(PCA9632_REG_MODE, PCA9632_FUNCTIONSET | (PCA9632_ROWS == 2 ? PCA9632_2LINE : PCA9632_1LINE), setbus); // & c == 0); // Only the first time
+    errlev = writeRegister(JHD1313_REG_MODE, JHD1313_FUNCTIONSET | (JHD1313_ROWS == 2 ? JHD1313_2LINE : JHD1313_1LINE), setbus); // & c == 0); // Only the first time
     // delayMicroseconds(c == 0 ? 4500 : 150);
   // } while(c < 3); // Don't know why exactly but we ought to do this 3 times
   I2CIP_ERR_BREAK(errlev);
     delayMicroseconds(4500);
 
-  errlev = writeRegister(PCA9632_REG_MODE, PCA9632_FUNCTIONSET | (PCA9632_ROWS == 2 ? PCA9632_2LINE : PCA9632_1LINE), false);
+  errlev = writeRegister(JHD1313_REG_MODE, JHD1313_FUNCTIONSET | (JHD1313_ROWS == 2 ? JHD1313_2LINE : JHD1313_1LINE), false);
   I2CIP_ERR_BREAK(errlev);
   delayMicroseconds(150);
 
-  errlev = writeRegister(PCA9632_REG_MODE, PCA9632_FUNCTIONSET | (PCA9632_ROWS == 2 ? PCA9632_2LINE : PCA9632_1LINE), false);
+  errlev = writeRegister(JHD1313_REG_MODE, JHD1313_FUNCTIONSET | (JHD1313_ROWS == 2 ? JHD1313_2LINE : JHD1313_1LINE), false);
   I2CIP_ERR_BREAK(errlev);
 
-  errlev = writeRegister(PCA9632_REG_MODE, PCA9632_FUNCTIONSET | (PCA9632_ROWS == 2 ? PCA9632_2LINE : PCA9632_1LINE), false);
+  errlev = writeRegister(JHD1313_REG_MODE, JHD1313_FUNCTIONSET | (JHD1313_ROWS == 2 ? JHD1313_2LINE : JHD1313_1LINE), false);
   I2CIP_ERR_BREAK(errlev);
 
   // DISPLAY
-  // errlev = writeRegister(PCA9632_REG_MODE, PCA9632_DISPLAYCONTROL | PCA9632_DISPLAYON | (PCA9632_ENABLE_CURSOR ? PCA9632_CURSORON : PCA9632_CURSOROFF) | (PCA9632_ENABLE_BLINK ? PCA9632_BLINKON : PCA9632_BLINKOFF), false);
-  errlev = writeRegister(PCA9632_REG_MODE, PCA9632_DISPLAYCONTROL | PCA9632_DISPLAYON | PCA9632_CURSORON, false);
+  // errlev = writeRegister(JHD1313_REG_MODE, JHD1313_DISPLAYCONTROL | JHD1313_DISPLAYON | (JHD1313_ENABLE_CURSOR ? JHD1313_CURSORON : JHD1313_CURSOROFF) | (JHD1313_ENABLE_BLINK ? JHD1313_BLINKON : JHD1313_BLINKOFF), false);
+  errlev = writeRegister(JHD1313_REG_MODE, JHD1313_DISPLAYCONTROL | JHD1313_DISPLAYON | JHD1313_CURSORON, false);
   I2CIP_ERR_BREAK(errlev);
-  delayMicroseconds(PCA9632_DELAY_MICROS);
+  delayMicroseconds(JHD1313_DELAY_MICROS);
 
   // // CLEAR
-  errlev = writeRegister(PCA9632_REG_MODE, PCA9632_CLEARDISPLAY, false);
+  errlev = writeRegister(JHD1313_REG_MODE, JHD1313_CLEARDISPLAY, false);
   I2CIP_ERR_BREAK(errlev);
-  delayMicroseconds(PCA9632_DELAY_MICROS);
+  delayMicroseconds(JHD1313_DELAY_MICROS);
 
   // ENTRY MODE L TO R
-  errlev = writeRegister(PCA9632_REG_MODE, PCA9632_ENTRYMODESET | PCA9632_ENTRYLEFT | PCA9632_ENTRYSHIFTDECREMENT, false);
+  errlev = writeRegister(JHD1313_REG_MODE, JHD1313_ENTRYMODESET | JHD1313_ENTRYLEFT | JHD1313_ENTRYSHIFTDECREMENT, false);
   I2CIP_ERR_BREAK(errlev);
-  delayMicroseconds(PCA9632_DELAY_MICROS);
+  delayMicroseconds(JHD1313_DELAY_MICROS);
 
-  errlev = writeRegister(PCA9632_REG_MODE, PCA9632_RETURNHOME, false);
+  errlev = writeRegister(JHD1313_REG_MODE, JHD1313_RETURNHOME, false);
   I2CIP_ERR_BREAK(errlev);
-  delayMicroseconds(PCA9632_DELAY_MICROS);
+  delayMicroseconds(JHD1313_DELAY_MICROS);
 
   // RGB
   errlev = Device::writeRegister(fqa_rgb, (uint8_t)0x0, (uint8_t)0x0, false); // Backlight
@@ -78,68 +82,68 @@ i2cip_errorlevel_t PCA9632::begin(uint8_t cols, uint8_t rows, bool setbus) {
   // return errlev;
 }
 
-i2cip_errorlevel_t PCA9632::setCursor(uint8_t col, uint8_t row, bool setbus) {
-  if(!initialized) return I2CIP_ERR_SOFT;
-  if(col > PCA9632_COLS || row > PCA9632_ROWS) {
+i2cip_errorlevel_t JHD1313::setCursor(uint8_t col, uint8_t row, bool setbus) {
+  // if(!this->initialized) return I2CIP_ERR_SOFT;
+  if(col > JHD1313_COLS || row > JHD1313_ROWS) {
     return I2CIP_ERR_SOFT;
   }
-  uint8_t b = (row == 0 ? (col | PCA9632_ROW1) : (col | (PCA9632_ROW2)));
-  return writeRegister(PCA9632_REG_MODE, b, setbus);
+  uint8_t b = (row == 0 ? (col | JHD1313_ROW1) : (col | (JHD1313_ROW2)));
+  return writeRegister(JHD1313_REG_MODE, b, setbus);
 }
 
-i2cip_errorlevel_t PCA9632::setRGB(uint8_t r, uint8_t g, uint8_t b, bool setbus) {
-  if(!initialized) return I2CIP_ERR_SOFT;
-  i2cip_errorlevel_t errlev = Device::writeRegister(fqa_rgb, PCA9632_REG_RED, r, setbus);
+i2cip_errorlevel_t JHD1313::setRGB(uint8_t r, uint8_t g, uint8_t b, bool setbus) {
+  // if(!this->initialized) return I2CIP_ERR_SOFT;
+  i2cip_errorlevel_t errlev = Device::writeRegister(fqa_rgb, JHD1313_REG_RED, r, setbus);
   I2CIP_ERR_BREAK(errlev);
-  errlev = Device::writeRegister(fqa_rgb, PCA9632_REG_GREEN, g, false);
+  errlev = Device::writeRegister(fqa_rgb, JHD1313_REG_GREEN, g, false);
   I2CIP_ERR_BREAK(errlev);
-  return Device::writeRegister(fqa_rgb, PCA9632_REG_BLUE, b, false);
+  return Device::writeRegister(fqa_rgb, JHD1313_REG_BLUE, b, false);
 }
 
-i2cip_errorlevel_t PCA9632::set(const String& buf_args, const i2cip_pca9632_args_t& args) {
+i2cip_errorlevel_t JHD1313::set(const String& buf_args, const i2cip_jhd1313_args_t& args) {
   i2cip_errorlevel_t errlev = I2CIP_ERR_NONE;
-  if(!initialized) {
+  if(!this->initialized) {
     delayMicroseconds(50000); // Wakeup delay
-    errlev = begin(PCA9632_COLS, PCA9632_ROWS, false);
+    errlev = begin(JHD1313_COLS, JHD1313_ROWS, false);
     I2CIP_ERR_BREAK(errlev);
 
     #ifdef I2CIP_DEBUG_SERIAL
       DEBUG_DELAY();
-      I2CIP_DEBUG_SERIAL.println(F("[PCA9632 | SET] INIT PASS"));
+      I2CIP_DEBUG_SERIAL.println(F("[JHD1313 | SET] INIT PASS"));
       DEBUG_DELAY();
     #endif
-    initialized = true;
-    delayMicroseconds(PCA9632_DELAY_MICROS);
+    this->initialized = true; // Comment out to always begin
+    delayMicroseconds(JHD1313_DELAY_MICROS);
   }
   // Clear
-  errlev = writeRegister(PCA9632_REG_MODE, PCA9632_CLEARDISPLAY, false);
-  I2CIP_ERR_BREAK(errlev);
-  delayMicroseconds(PCA9632_DELAY_MICROS);
+  errlev = writeRegister(JHD1313_REG_MODE, JHD1313_CLEARDISPLAY, false);
+  if(errlev != I2CIP_ERR_NONE) { this->initialized = false; return errlev; }
+  delayMicroseconds(JHD1313_DELAY_MICROS);
 
   errlev = setRGB(args.r, args.g, args.b, false);
-  I2CIP_ERR_BREAK(errlev);
+  if(errlev != I2CIP_ERR_NONE) { this->initialized = false; return errlev; }
 
   errlev = setCursor(0, 0, false);
-  I2CIP_ERR_BREAK(errlev);
+  if(errlev != I2CIP_ERR_NONE) { this->initialized = false; return errlev; }
   #ifdef I2CIP_DEBUG_SERIAL
     DEBUG_DELAY();
-    I2CIP_DEBUG_SERIAL.println(F("[PCA9632 | SET] SCREEN CLEAR; RGB SET; CURSOR RESET; PASS"));
+    I2CIP_DEBUG_SERIAL.println(F("[JHD1313 | SET] SCREEN CLEAR; RGB SET; CURSOR RESET; PASS"));
     DEBUG_DELAY();
   #endif
-  delayMicroseconds(PCA9632_DELAY_MICROS);
+  delayMicroseconds(JHD1313_DELAY_MICROS);
 
-  String buf = String("[I2CIP] HELLO!\n") + fqaToString(this->fqa); // hardcode failsafe
+  String buf = String("[I2CIP] HELLO :D\n") + fqaToString(this->fqa); // hardcode failsafe
   // String buf = String(buf_args.c_str()); // Copy
   // if(buf_stream.write('\n') != 1) { return I2CIP_ERR_SOFT; }
-  size_t len = buf.length(); // strlen(buf);
-  if(len <= 0) return I2CIP_ERR_SOFT;
-  if(len > PCA9632_TXMAX) {
-    len = PCA9632_TXMAX;
+  size_t len = buf_args.length(); // strlen(buf);
+  if(len > 0) buf = buf_args;
+  if(len > JHD1313_TXMAX) {
+    len = JHD1313_TXMAX;
   }
 
   #ifdef I2CIP_DEBUG_SERIAL
     DEBUG_DELAY();
-    I2CIP_DEBUG_SERIAL.print(F("[PCA9632 | SET] PRINT ARGS '"));
+    I2CIP_DEBUG_SERIAL.print(F("[JHD1313 | SET] PRINT ARGS '"));
     I2CIP_DEBUG_SERIAL.print(buf);
     I2CIP_DEBUG_SERIAL.println(F("'"));
     DEBUG_DELAY();
@@ -150,7 +154,7 @@ i2cip_errorlevel_t PCA9632::set(const String& buf_args, const i2cip_pca9632_args
   // uint8_t s = 0; // row shift
   // #ifdef I2CIP_DEBUG_SERIAL
   //   DEBUG_DELAY();
-  //   I2CIP_DEBUG_SERIAL.print(F("[PCA9632 | SET] BUFFER '"));
+  //   I2CIP_DEBUG_SERIAL.print(F("[JHD1313 | SET] BUFFER '"));
   //   I2CIP_DEBUG_SERIAL.print(buf);
   //   I2CIP_DEBUG_SERIAL.print(F("' -> '"));
   // #endif
@@ -159,12 +163,12 @@ i2cip_errorlevel_t PCA9632::set(const String& buf_args, const i2cip_pca9632_args
   //     #ifdef I2CIP_DEBUG_SERIAL
   //       I2CIP_DEBUG_SERIAL.print("\\n");
   //     #endif
-  //     s++; if(s >= PCA9632_ROWS) { // increment row shifter
+  //     s++; if(s >= JHD1313_ROWS) { // increment row shifter
   //       return errlev;
   //     }
 
   //     // Write and clear line
-  //     // errlev = writeRegister(PCA9632_REG_DATA, (uint8_t*)(&_buf[last]), (size_t)PCA9632_COLS, false);
+  //     // errlev = writeRegister(JHD1313_REG_DATA, (uint8_t*)(&_buf[last]), (size_t)JHD1313_COLS, false);
   //     // I2CIP_ERR_BREAK(errlev);
 
   //     // Set cursor
@@ -189,7 +193,7 @@ i2cip_errorlevel_t PCA9632::set(const String& buf_args, const i2cip_pca9632_args
   //   }
   //   // _buf[i-s] = buf.charAt(i); //[i];
   // }
-  // // errlev = writeRegister(PCA9632_REG_DATA, (uint8_t*)(&_buf[last]), PCA9632_TXMAX - last, false); // Write all bytes (clears display)
+  // // errlev = writeRegister(JHD1313_REG_DATA, (uint8_t*)(&_buf[last]), JHD1313_TXMAX - last, false); // Write all bytes (clears display)
   // // I2CIP_ERR_BREAK(errlev);
   // #ifdef I2CIP_DEBUG_SERIAL
   //   I2CIP_DEBUG_SERIAL.println(F("' PASS"));
@@ -200,16 +204,16 @@ i2cip_errorlevel_t PCA9632::set(const String& buf_args, const i2cip_pca9632_args
   // I2CIP_ERR_BREAK(errlev);
   // #ifdef I2CIP_DEBUG_SERIAL
   //   DEBUG_DELAY();
-  //   I2CIP_DEBUG_SERIAL.println(F("[PCA9632 | SET] RGB PASS"));
+  //   I2CIP_DEBUG_SERIAL.println(F("[JHD1313 | SET] RGB PASS"));
   //   DEBUG_DELAY();
   // #endif
 
   // // Enable display
-  // return writeRegister(PCA9632_REG_MODE, PCA9632_DISPLAYCONTROL | PCA9632_DISPLAYON, false);
+  // return writeRegister(JHD1313_REG_MODE, JHD1313_DISPLAYCONTROL | JHD1313_DISPLAYON, false);
   // I2CIP_ERR_BREAK(errlev);
 }
 
-size_t PCA9632::write(uint8_t b) {
+size_t JHD1313::write(uint8_t b) {
   size_t r = 0;
   switch(b) {
     case '\0':
@@ -218,9 +222,9 @@ size_t PCA9632::write(uint8_t b) {
       r = (setCursor(0, 1, false) == I2CIP_ERR_NONE ? 1 : 0); // Hard-coded Line 2 Set
       break;
     default:
-      r = (writeRegister(PCA9632_REG_DATA, b, false) == I2CIP_ERR_NONE ? 1 : 0);
+      r = (writeRegister(JHD1313_REG_DATA, b, false) == I2CIP_ERR_NONE ? 1 : 0);
       break;
   }
-  delayMicroseconds(PCA9632_DELAY_MICROS);
+  delayMicroseconds(JHD1313_DELAY_MICROS);
   return r;
 }
--- src/JHD1313.h
+++ src/JHD1313.h
@@ -1,5 +1,5 @@
-#ifndef I2CIP_PCA9632_H_
-#define I2CIP_PCA9632_H_
+#ifndef I2CIP_JHD1313_H_
+#define I2CIP_JHD1313_H_
 
 #include <Arduino.h>
 #include <I2CIP.h>
@@ -11,78 +11,78 @@
 // #define BLUE            3
 
 // commands
-#define PCA9632_CLEARDISPLAY (uint8_t)0x01
-#define PCA9632_RETURNHOME (uint8_t)0x02
-#define PCA9632_ENTRYMODESET (uint8_t)0x04
-#define PCA9632_DISPLAYCONTROL (uint8_t)0x08
-#define PCA9632_CURSORSHIFT (uint8_t)0x10
-#define PCA9632_FUNCTIONSET (uint8_t)0x20
-#define PCA9632_SETCGRAMADDR (uint8_t)0x40
-#define PCA9632_SETDDRAMADDR (uint8_t)0x80
+#define JHD1313_CLEARDISPLAY (uint8_t)0x01
+#define JHD1313_RETURNHOME (uint8_t)0x02
+#define JHD1313_ENTRYMODESET (uint8_t)0x04
+#define JHD1313_DISPLAYCONTROL (uint8_t)0x08
+#define JHD1313_CURSORSHIFT (uint8_t)0x10
+#define JHD1313_FUNCTIONSET (uint8_t)0x20
+#define JHD1313_SETCGRAMADDR (uint8_t)0x40
+#define JHD1313_SETDDRAMADDR (uint8_t)0x80
 
 // flags for display entry mode
-#define PCA9632_ENTRYRIGHT (uint8_t)0x00
-#define PCA9632_ENTRYLEFT (uint8_t)0x02
-#define PCA9632_ENTRYSHIFTINCREMENT (uint8_t)0x01
-#define PCA9632_ENTRYSHIFTDECREMENT (uint8_t)0x00
+#define JHD1313_ENTRYRIGHT (uint8_t)0x00
+#define JHD1313_ENTRYLEFT (uint8_t)0x02
+#define JHD1313_ENTRYSHIFTINCREMENT (uint8_t)0x01
+#define JHD1313_ENTRYSHIFTDECREMENT (uint8_t)0x00
 
 // flags for display on/off control
-#define PCA9632_DISPLAYON (uint8_t)0x04
-#define PCA9632_DISPLAYOFF (uint8_t)0x00
-#define PCA9632_CURSORON (uint8_t)0x02
-#define PCA9632_CURSOROFF (uint8_t)0x00
-#define PCA9632_BLINKON (uint8_t)0x01
-#define PCA9632_BLINKOFF (uint8_t)0x00
+#define JHD1313_DISPLAYON (uint8_t)0x04
+#define JHD1313_DISPLAYOFF (uint8_t)0x00
+#define JHD1313_CURSORON (uint8_t)0x02
+#define JHD1313_CURSOROFF (uint8_t)0x00
+#define JHD1313_BLINKON (uint8_t)0x01
+#define JHD1313_BLINKOFF (uint8_t)0x00
 
 // flags for display/cursor shift
-#define PCA9632_DISPLAYMOVE (uint8_t)0x08
-#define PCA9632_CURSORMOVE (uint8_t)0x00
-#define PCA9632_MOVERIGHT (uint8_t)0x04
-#define PCA9632_MOVELEFT (uint8_t)0x00
+#define JHD1313_DISPLAYMOVE (uint8_t)0x08
+#define JHD1313_CURSORMOVE (uint8_t)0x00
+#define JHD1313_MOVERIGHT (uint8_t)0x04
+#define JHD1313_MOVELEFT (uint8_t)0x00
 
 // flags for function set
-#define PCA9632_8BITMODE (uint8_t)0x10
-#define PCA9632_4BITMODE (uint8_t)0x00
-#define PCA9632_2LINE (uint8_t)0x08
-#define PCA9632_1LINE (uint8_t)0x00
-#define PCA9632_5x10DOTS (uint8_t)0x04
-#define PCA9632_5x8DOTS (uint8_t)0x00
+#define JHD1313_8BITMODE (uint8_t)0x10
+#define JHD1313_4BITMODE (uint8_t)0x00
+#define JHD1313_2LINE (uint8_t)0x08
+#define JHD1313_1LINE (uint8_t)0x00
+#define JHD1313_5x10DOTS (uint8_t)0x04
+#define JHD1313_5x8DOTS (uint8_t)0x00
 
 // Registers
-#define PCA9632_REG_MODE (uint8_t)0x80
-#define PCA9632_REG_DATA (uint8_t)0x40
-#define PCA9632_REG_RED (uint8_t)0x04
-#define PCA9632_REG_GREEN (uint8_t)0x03
-#define PCA9632_REG_BLUE (uint8_t)0x02
-#define PCA9632_ROW1 (uint8_t)0x80
-#define PCA9632_ROW2 (uint8_t)0xc0
+#define JHD1313_REG_MODE (uint8_t)0x80
+#define JHD1313_REG_DATA (uint8_t)0x40
+#define JHD1313_REG_RED (uint8_t)0x04
+#define JHD1313_REG_GREEN (uint8_t)0x03
+#define JHD1313_REG_BLUE (uint8_t)0x02
+#define JHD1313_ROW1 (uint8_t)0x80
+#define JHD1313_ROW2 (uint8_t)0xc0
 
 // Device I2C Arress
-#define I2CIP_PCA9632_ADDRESS     0x3e
-#define PCA9632_ADDRESS_RGBLED     0x62
+#define I2CIP_JHD1313_ADDRESS     0x3e
+#define JHD1313_ADDRESS_RGBLED     0x62
 // #define RGB_ADDRESS_V5  (0x30)
-#define PCA9632_ROWS 2
-#define PCA9632_COLS 16
-#define PCA9632_TXMAX PCA9632_ROWS * PCA9632_COLS
+#define JHD1313_ROWS 2
+#define JHD1313_COLS 16
+#define JHD1313_TXMAX JHD1313_ROWS * JHD1313_COLS
 
-#define PCA9632_ENABLE_CURSOR true // Uncomment to enable cursor
-#define PCA9632_ENABLE_BLINK true // Uncomment to enable cursor blinking
+#define JHD1313_ENABLE_CURSOR true // Uncomment to enable cursor
+#define JHD1313_ENABLE_BLINK true // Uncomment to enable cursor blinking
 
-#define PCA9632_DELAY_MICROS 2000
+#define JHD1313_DELAY_MICROS 2000
 
-// const char i2cip_pca9632_id_progmem[] PROGMEM = {"PCA9632"};
+// const char i2cip_jhd1313_id_progmem[] PROGMEM = {"JHD1313"};
 
 typedef struct {
   uint8_t r;
   uint8_t g;
   uint8_t b;
-} i2cip_pca9632_args_t;
+} i2cip_jhd1313_args_t;
 
-class PCA9632 : public I2CIP::Device, public I2CIP::OutputInterface<String, i2cip_pca9632_args_t>, private Print {
-  I2CIP_DEVICE_CLASS_BUNDLE(PCA9632);
-  I2CIP_OUTPUT_USE_FAILSAFE(String, i2cip_pca9632_args_t);
+class JHD1313 : public I2CIP::Device, public I2CIP::OutputInterface<String, i2cip_jhd1313_args_t>, private Print {
+  I2CIP_DEVICE_CLASS_BUNDLE(JHD1313);
+  I2CIP_OUTPUT_USE_FAILSAFE(String, i2cip_jhd1313_args_t);
   private:
-    // PCA9632(i2cip_fqa_t fqa) : I2CIP::Device(fqa, i2cip_pca9632_id_progmem, _id), I2CIP::OutputInterface<uint8_t*, size_t>((I2CIP::Device*)this) { }
+    // JHD1313(i2cip_fqa_t fqa) : I2CIP::Device(fqa, i2cip_jhd1313_id_progmem, _id), I2CIP::OutputInterface<uint8_t*, size_t>((I2CIP::Device*)this) { }
     bool initialized = false;
 
     const i2cip_fqa_t fqa_rgb;
@@ -91,13 +91,13 @@ class PCA9632 : public I2CIP::Device, public I2CIP::OutputInterface<String, i2ci
     i2cip_errorlevel_t setCursor(uint8_t col, uint8_t row, bool setbus = false);
     i2cip_errorlevel_t setRGB(uint8_t r, uint8_t g, uint8_t b, bool setbus = false);
     
-    PCA9632(i2cip_fqa_t fqa, const i2cip_id_t& id);
+    JHD1313(i2cip_fqa_t fqa, const i2cip_id_t& id);
 
     virtual size_t write(uint8_t b) override;
   public:
 
-    i2cip_errorlevel_t set(const String& buf, const i2cip_pca9632_args_t& len);
-    // i2cip_errorlevel_t set(const char* const& buf, const i2cip_pca9632_args_t& len);
+    i2cip_errorlevel_t set(const String& buf, const i2cip_jhd1313_args_t& len);
+    // i2cip_errorlevel_t set(const char* const& buf, const i2cip_jhd1313_args_t& len);
 };
 
 #endif
